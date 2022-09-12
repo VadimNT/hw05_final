ebf103b3e8c6641ff910f569ecc43598a9045871
VadimNT <smil_e@mail.ru>
2022-09-12 10:41:40 +0500

    v.3 Исправил миксины для тестов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,12 +9,6 @@ from posts.forms import CommentForm, PostForm
 from posts.models import Comment, Follow, Group, Post, User
 
 
-# def index(request):
-#     posts = Post.objects.prefetch_related("author", "group")
-#     page_obj = get_paginator(request, posts)
-#     return render(request, "posts/index.html", {"page_obj": page_obj})
-
-
 class IndexView(ListView):
     template_name = "posts/index.html"
     paginate_by = settings.NUMS_OF_POSTS_ON_PAGE
@@ -22,17 +16,6 @@ class IndexView(ListView):
 
     def get_queryset(self):
         return self.model.objects.prefetch_related("author", "group")
-
-
-# def group_posts(request, slug):
-#     group = get_object_or_404(Group, slug=slug)
-#     posts = group.posts.select_related("author")
-#     page_obj = get_paginator(request, posts)
-#     return render(
-#         request,
-#         "posts/group_list.html",
-#         {"group": group, "page_obj": page_obj},
-#     )
 
 
 class GroupPostsView(ListView):
@@ -54,21 +37,6 @@ class GroupPostsView(ListView):
         )
         return context
 
-
-# def profile(request, username):
-#     author = get_object_or_404(User, username=username)
-#     posts = author.posts.prefetch_related("group")
-#     following = False
-#     if request.user.is_authenticated:
-#         following = Follow.objects.filter(
-#             user=request.user, author=author
-#         ).exists()
-#     page_obj = get_paginator(request, posts)
-#     return render(
-#         request,
-#         "posts/profile.html",
-#         {"page_obj": page_obj, "author": author, "following": following},
-#     )
 
 class ProfileView(ListView):
     model = Post
@@ -103,26 +71,6 @@ class ProfileView(ListView):
         return context
 
 
-# def post_detail(request, post_id):
-#     post = get_object_or_404(Post, pk=post_id)
-#     count = Post.objects.filter(author=post.author).count()
-#     comments = post.comments.all()
-#     form_comment = CommentForm(request.POST or None)
-#     if form_comment.is_valid():
-#         form_comment.save()
-#         return redirect("posts:post_detail", post.id)
-#     return render(
-#         request,
-#         "posts/post_detail.html",
-#         {
-#             "count": count,
-#             "post": post,
-#             "comments": comments,
-#             "form_comment": form_comment,
-#         },
-#     )
-
-
 class PostDetailView(DetailView):
     model = Post
     template_name = "posts/post_detail.html"
@@ -139,17 +87,6 @@ class PostDetailView(DetailView):
         ).all()
         context["form_comment"] = CommentForm()
         return context
-
-
-# @login_required
-# def post_create(request):
-#     form = PostForm(request.POST or None, files=request.FILES or None)
-#     if form.is_valid():
-#         post = form.save(commit=False)
-#         post.author = request.user
-#         post.save()
-#         return redirect("posts:profile", request.user)
-#     return render(request, "posts/create_post.html", {"form": form})
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -170,22 +107,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
             return redirect("posts:profile", request.user)
         self.context["form"] = form
         return render(request, self.template_name, self.context)
-
-
-# @login_required
-# def post_edit(request, post_id):
-#     post = get_object_or_404(Post, pk=post_id)
-#     if request.user != post.author:
-#         return redirect("posts:post_detail", post.id)
-#     form = PostForm(
-#         request.POST or None, files=request.FILES or None, instance=post
-#     )
-#     if form.is_valid():
-#         form.save()
-#         return redirect("posts:post_detail", post.id)
-#     return render(
-#         request, "posts/create_post.html", {"post": post, "form": form}
-#     )
 
 
 class PostEditView(LoginRequiredMixin, UpdateView):
@@ -209,18 +130,6 @@ class PostEditView(LoginRequiredMixin, UpdateView):
         return reverse(
             "posts:post_detail", kwargs={"post_id": self.kwargs["post_id"]}
         )
-
-
-# @login_required
-# def add_comment(request, post_id):
-#     post = get_object_or_404(Post, pk=post_id)
-#     form = CommentForm(request.POST or None)
-#     if form.is_valid():
-#         comment = form.save(commit=False)
-#         comment.author = request.user
-#         comment.post = post
-#         comment.save()
-#     return redirect("posts:post_detail", post_id=post_id)
 
 
 class AddCommentView(LoginRequiredMixin, CreateView):
@@ -250,13 +159,6 @@ class AddCommentView(LoginRequiredMixin, CreateView):
         return redirect("posts:post_detail", post_id=self.kwargs["post_id"])
 
 
-# @login_required
-# def follow_index(request):
-#     posts = Post.objects.filter(author__following__user=request.user)
-#     page_obj = get_paginator(request, posts)
-#     return render(request, "posts/follow.html", {"page_obj": page_obj})
-
-
 class FollowIndexView(LoginRequiredMixin, ListView):
     model = Post
     paginate_by = settings.NUMS_OF_POSTS_ON_PAGE
@@ -272,30 +174,12 @@ class FollowIndexView(LoginRequiredMixin, ListView):
         return self.model.objects.filter(author__following__user=self.user)
 
 
-# @login_required
-# def profile_follow(request, username):
-#     # Подписаться на автора
-#     author = get_object_or_404(User, username=username)
-#     if request.user != author:
-#         Follow.objects.get_or_create(user=request.user, author=author)
-#     return redirect("posts:profile", username=username)
-
-
 class ProfileFollowView(LoginRequiredMixin, View):
     def get(self, request, **kwargs):
         author = get_object_or_404(User, username=self.kwargs["username"])
         if request.user != author:
             Follow.objects.get_or_create(user=request.user, author=author)
         return redirect("posts:profile", username=self.kwargs["username"])
-
-
-# @login_required
-# def profile_unfollow(request, username):
-#     # Дизлайк, отписка
-#     author = get_object_or_404(User, username=username)
-#     if request.user != author:
-#         Follow.objects.filter(user=request.user, author=author).delete()
-#     return redirect("posts:profile", username=username)
 
 
 class ProfileUnfollowView(LoginRequiredMixin, View):
